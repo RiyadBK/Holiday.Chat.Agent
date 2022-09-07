2ae123b1f3260325ed25c7545fae2c0417b99e1b
Riyad Boksh <riyad.boksh@youlend.com>
2022-09-07 21:13:05 +0100

    Add array of holiday places

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -7,9 +7,66 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChatComponent implements OnInit {
 
+  holidayRecommendations = 
+  [
+    "Uptown at Bali, Indonesia",
+    "Relaxamax at New Orleans, USA",
+    "WindyBeach at Ventry, Ireland",
+    "Twighlight at Marrakech, Morocco",
+    "TooHot at Sydney, Australia",
+    "Castaway at The Maldives",
+    "Eiffel Park at Paris, France",
+    "The Cape at Cape Town, South Africa",
+    "Desert Dreams at Dubai, United Arab Emirates",
+    "Sea Views at Bora Bora, French Polynesia",
+    "Apple City at New York, U.S.A",
+    "Island Hopper at Dubrovnik, Croatia",
+    "Castle Town at Edinburgh, Scotland",
+    "Wine Valley at Rome, Italy",
+    "Weary Traveller at Paro Valley, Bhutan",
+    "Hot Times at Jaipur, India",
+    "Forest Retreat at Waikato, New Zealand",
+    "Casablance at Havana, Cuba",
+    "Tech City at Tokyo, Japan",
+    "Ice Hotel at Base Marambio, Antartica",
+    "NorthStar at Greenland",
+  ];
+  questionNumber = 0;
+  firstQuestion: boolean = false;
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  startQuestions() {
+
+  }
+
+  checkRecommendations() {
+    if (this.holidayRecommendations.length = 1) {
+      this.showFinalRecommendation()
+    }
+  }
+
+  showFinalRecommendation() {
+    this.holidayRecommendations = this.holidayRecommendations;
+  }
+
+  firstQuestionAsked(msg: string) {
+    const index: number = this.holidayRecommendations.indexOf(msg);
+
+    if(this.firstQuestion = true) {
+      this.holidayRecommendations.splice(index, 1)
+      console.log(this.holidayRecommendations)
+    }
+  }
+
+  secondQuestionAsked() {
+
+  }
+
+  thirdQuestionAsked() {
+
+  }
 }
